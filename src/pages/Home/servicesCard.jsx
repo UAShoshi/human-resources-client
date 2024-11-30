f2f3b4f3c0daab611a68cd2d74c4b9576c1141ae
UAShoshi <ummeaymonafifha@gmail.com>
2024-11-30 21:31:45 +0600

    reset project

--- src/pages/Home/servicesCard.jsx
+++ src/pages/Home/servicesCard.jsx
@@ -18,4 +18,24 @@ const ServicesCard = ({service}) => {
   );
 };
 
+// https://ibb.co.com/tZXvfW3
+// https://ibb.co.com/MBRyQMz
+// https://ibb.co.com/XYcmhqQ
+// https://ibb.co.com/Fn1qpK2
+// https://ibb.co.com/h2Q1msh
+// https://ibb.co.com/PzR8VGv
+
+// https://ibb.co.com/QHWV7GM
+// https://ibb.co.com/thLTqpY
+// https://ibb.co.com/B3HwBBk
+// https://ibb.co.com/XZh3RPt
+// https://ibb.co.com/y5cScmN
+// https://ibb.co.com/6DmsL0X
+// https://ibb.co.com/5rYRPqD
+// https://ibb.co.com/GJX5MPw
+// https://ibb.co.com/brjkDWF
+// https://ibb.co.com/RS5tnkQ
+// https://ibb.co.com/nfZ2YF1
+// https://ibb.co.com/27Hy3C5
+
 export default ServicesCard;
